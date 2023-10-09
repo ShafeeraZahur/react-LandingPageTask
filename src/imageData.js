d7fec1e213a827a72f2ba1bde4d32ba3319e7e47
shafeerashafu <shafeerazahur3@gmail.com>
2023-10-09 20:51:51 +0530

    Commit added

---
+++ src/imageData.js
@@ -0,0 +1,16 @@
+// import Bgmasthead from '.../assets/img/Bgmasthead.jpg';
+// import Bgshowcase1 from '.../assets/img/Bgshowcase1.jpg';
+// import Bgshowcase2 from '.../assets/img/Bgshowcase2.jpg';
+// import Bgshowcase3 from '.../assets/img/Bgshowcase3.jpg';
+// import Testimonials1 from '.../assets/img/Testimonials1.jpg';
+// import Testimonials2 from '.../assets/img/Testimonials2.jpg';
+// import Testimonials3 from '.../assets/img/Testimonials3.jpg';
+
+
+// // eslint-disable-next-line
+
+// export const imgdata= [,Bgmasthead,Bgshowcase1,Bgshowcase2,Bgshowcase3,Testimonials1,Testimonials2,Testimonials3]
+
+// export const data= [
+//     {id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7}
+// ]
